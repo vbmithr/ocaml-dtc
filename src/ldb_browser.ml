302f98265b86ecea524ae4f8f2e9d72502caaf52
Vincent Bernardoff <vb@luminar.eu.org>
2016-06-28 10:35:39 +0200

    ldb_browser: use Command

--- src/ldb_browser.ml
+++ src/ldb_browser.ml
@@ -1,34 +1,33 @@
 open Core.Std
 
-let main max_ticks tail db =
+let main tail max_ticks dbpath () =
   let buf = Bytes.create 24 in
   let nb_read = ref 0 in
   let f = if tail then LevelDB.rev_iter else LevelDB.iter in
-  f (fun ts data ->
-      String.blit ts 0 buf 0 8;
-      String.blit data 0 buf 8 16;
-      let tick = Tick.IO.Bytes.read_tdts buf 0 in
-      Format.(fprintf std_formatter "%d %a@." !nb_read Tick.pp tick);
-      if max_ticks <> -1 && !nb_read = max_ticks
-      then (incr nb_read; false)
-      else (incr nb_read; true)
-    ) db
-
-let () =
-  let max_ticks = ref @@ -1 in
-  let tail = ref false in
-  let fn = ref "" in
-  let speclist = Arg.(align [
-      "-tail", Set tail, " Show latest records";
-      "-n", Set_int max_ticks, "<int> Number of ticks to display (default: all)";
-    ]) in
-  let anon_fun arg = fn := arg in
-  let usage_msg = "Usage: " ^ Sys.argv.(0) ^ " [options]\nOptions are:" in
-  Arg.parse speclist anon_fun usage_msg;
-  let main = main !max_ticks !tail in
-  let db = LevelDB.open_db !fn in
+  let db = LevelDB.open_db dbpath in
   try
-    main db;
-    LevelDB.close db;
+    f (fun ts data ->
+        String.blit ts 0 buf 0 8;
+        String.blit data 0 buf 8 16;
+        let tick = Tick.IO.Bytes.read_tdts buf 0 in
+        Format.(fprintf std_formatter "%d %a@." !nb_read Tick.pp tick);
+        incr nb_read;
+        Option.value_map max_ticks
+          ~default:true ~f:(fun max_ticks -> not (!nb_read = max_ticks))
+      ) db;
+    LevelDB.close db
   with _ -> LevelDB.close db
+
+let command =
+  let spec =
+    let open Command.Spec in
+    empty
+    +> flag "-tail" no_arg ~doc:" Show latest records"
+    +> flag "-n" (optional int) ~doc:"n Number of ticks to display (default: all)"
+    +> anon ("db" %: string)
+  in
+  Command.basic ~summary:"Browser for tick files" spec main
+
+let () = Command.run command
+
 
