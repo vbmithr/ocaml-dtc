e8226aec95df38c484b105590296c1fe4d7dd20c
Vincent Bernardoff <vb@luminar.eu.org>
2017-03-25 22:46:24 +0100

    buildsystem: fixups

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -4,12 +4,14 @@
 open Topkg
 
 let leveldb = Conf.with_pkg "leveldb"
+let async = Conf.with_pkg "async"
 
 let () =
   Pkg.describe "dtc" @@ fun c ->
   let leveldb = Conf.value c leveldb in
+  let async = Conf.value c async in
   Ok [
     Pkg.mllib "src/dtc.mllib" ;
     Pkg.bin ~cond:leveldb "src/ldb_browser" ;
-    Pkg.bin "src/hddownload" ;
+    Pkg.bin ~cond:async "src/hddownload" ;
   ]
